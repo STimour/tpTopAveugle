20d8f4a9dcda7de71d5705ce0e77699fa6dd2684
Alexbcda <alexandrebreso@gmail.com>
2023-11-23 12:21:22 +0100

    Depot Alex

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Timer from './components/Timer';
+import Button from './components/Button';
+import Result from './components/Result';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [endTime, setEndTime] = useState<number | null>(null);
+
+  const handleStart = () => {
+    setStartTime(Date.now());
+  };
+
+  const handleButtonClick = () => {
+    if (startTime !== null) {
+      setEndTime(Date.now());
+    }
+  };
+
+  const calculateError = () => {
+    if (startTime !== null && endTime !== null) {
+      const targetTime = 12; // Durée visée en secondes
+      const elapsedTime = Math.abs((endTime - startTime) / 1000);
+      return Math.abs(targetTime - elapsedTime);
+    }
+    return null;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Timer duration={12} onTimeUp={() => setEndTime(Date.now())} />
+      <Button onStart={handleStart} onClick={handleButtonClick} />
+      <Result error={calculateError()} />
+    </div>
+  );
 }
 
-export default App
+export default App;

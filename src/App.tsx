0f343398b041cf2b97b6d210ed38b5057a2139dc
calanfive <camille.lelan@gmail.com>
2023-11-24 16:48:47 +0100

    fix all

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,22 @@ import Result from './components/Display'
 import './App.css';
 
 function App() {
-  const [startTime, setStartTime] = useState<Date | null>(null);
+  const [startTime, setStartTime] = useState<number | null>(null);
   
-  const [endTime, setEndTime] = useState<Date>(new Date());
+  const [endTime, setEndTime] = useState<number | null>(null);
 
   const [targerTime, setTargetTime] = useState(Math.floor(Math.random() * 16) + 5)
 
-  const [timeToWin, setTimeToWin] = useState('')
+  const [timeToWin, setTimeToWin] = useState<number | null>(null)
 
   const handleStart = () => {
-    setStartTime(new Date());
-    setTimeToWin(new Date(Number(startTime) + targerTime * 1000).toLocaleTimeString())
+    const startTime = Date.now()
+    setStartTime(startTime);
+    setTimeToWin(startTime + targerTime * 1000)
   };
 
   const handleButtonClick = () => {
-      setEndTime(new Date());
+      setEndTime(Date.now());
   };
 
   
@@ -28,9 +29,8 @@ function App() {
   return (
     <div className="App">
       <Timer numberAleat={targerTime}/>
-      <h1>Début à : {startTime !== undefined ? startTime?.toLocaleTimeString() : 0}</h1>
-      <Result numberAleat={targerTime} timeToWin={timeToWin}/>
-      <Button onStart={()=> handleStart()} onEnd={() => handleButtonClick}/>
+      <Result numberAleat={targerTime} timeToWin={timeToWin} end={endTime}/>
+      <Button onStart={()=> handleStart()} onEnd={() => handleButtonClick()}/>
     </div>
   );
 }
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,15 +1,23 @@
+import { useMemo } from "react";
+
 type Resultat = {
     numberAleat: number,
-    timeToWin: string
+    timeToWin: number | null,
+    end: number | null
 }
 
 const Display = (props: Resultat) => {
-    const {numberAleat, timeToWin} = props
+    const {numberAleat, timeToWin, end} = props
+
+    const result = useMemo(
+        () => (end && timeToWin) ? Math.abs(timeToWin - end) : 0
+    , [timeToWin, end])
           
     return (
         <div className="affichage">
             <p>Objectif : {numberAleat}</p>
             <p>Resultat attendu: {timeToWin}</p>
+            <p>Différence: {result}</p>
         </div>
     );
 };
